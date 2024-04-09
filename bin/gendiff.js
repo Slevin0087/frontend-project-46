ae911d4b71e2fee4c894877faac9cb1cce6d35ba
Slevin <abaza-09@mail.ru>
2024-04-09 13:36:40 +0300

    add modified README

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -29,9 +29,9 @@ program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
-  .option('-f, --format <type>', 'output format', plain)
+  .option('-f, --format <type>', 'output format', 'plain')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => {
+  .action((filepath1, filepath2, options) => {
     // const parses = parse(filepath1, filepath2);
     // // console.log(parses);
     // const diff = createTree(parses);
@@ -46,7 +46,7 @@ program
     // const jsonFormatter = formatterJson(diff);
     // console.log(jsonFormatter);
     // const form = program.opts;
-    console.log(diff(filepath1, filepath2));
+    console.log(diff(filepath1, filepath2, options.format));
     // console.log(diff(filepath1, filepath2, program.opts().format));
   });
 
