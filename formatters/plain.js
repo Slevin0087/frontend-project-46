f82fe54f2f75f671ff0bee3c9e7c87a12a482977
Slevin <abaza-09@mail.ru>
2024-04-10 21:06:58 +0300

    add modified

--- formatters/plain.js
+++ formatters/plain.js
@@ -1,6 +1,4 @@
 const typeValue = (value) => {
-  // const len = value.length;
-  // console.log(len);
   const type = typeof value;
   if (type === 'object' && value !== null) {
     return '[complex value]';
@@ -13,35 +11,20 @@ const typeValue = (value) => {
   }
   return value;
 };
-// console.log('typeValue:', typeValue(''));
 
 const plain = (arr, paths = '') => {
   let steck = '';
   let reupdateVal;
   arr.forEach((obj) => {
-    // const {
-    //   type, key, val, children,
-    // } = obj;
-    // console.log('val:', val);
-    // const valType = typeof val === 'string' ? `'${val}'` : val;
-    // console.log('valType:', valType);
-    // const typeVal = typeof valType === 'object' && valType !== null ? '[complex value]' : valType;
     let path = paths;
     if (obj.type === 'added') {
       path += path.length ? `${'.'}${obj.key}` : obj.key;
       steck += `${'Property'} '${path}' ${'was added with value:'} ${typeValue(obj.val)}\n`;
     }
     if (obj.type === 'reupdated') {
-      // console.log('val:', obj.val);
-      // console.log('typeval:', typeof obj.val);
-
       reupdateVal = typeValue(obj.val);
-      // console.log("val === '' ?", reupdateVal === obj.val);
-      // typeValue(obj.val === `${''}` ? '' : obj.val);
-      // console.log('reupdateVal:', reupdateVal);
     }
     if (obj.type === 'updated') {
-      // console.log('reupdateValUpdated:', reupdateVal);
       path += path.length ? `${'.'}${obj.key}` : obj.key;
       steck += `${'Property'} '${path}' ${'was updated. From'} ${reupdateVal} ${'to'} ${typeValue(obj.val)}\n`;
     }
