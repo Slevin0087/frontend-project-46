9555ff61a61207069eda63dcec0bfce256965484
Slevin <abaza-09@mail.ru>
2024-04-10 12:44:34 +0300

    add modified

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,5 +1,5 @@
 import json from './json.js';
 
-const stylish = (arr) => JSON.stringify(json(arr), null, 2).replace(/("|'|,)/gm, '');
+const stylish = (arr) => JSON.stringify(json(arr), null, 3).replace(/("|'|,)/gm, '');
 
 export default stylish;
