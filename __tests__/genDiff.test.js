a301e778349036cb9205141203a62bd661e809b0
Slevin <abaza-09@mail.ru>
2024-04-13 22:54:07 +0300

    modified

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -10,42 +10,42 @@ const fixturePath = (fileName) => {
 
 const readFile = (filename) => readFileSync(fixturePath(filename), 'utf-8');
 
-const stylishData = readFile('expectedStylish.txt'.trim());
-const stylishDataYaml = readFile('expectedstylish.txt'.trim());
-const plainData = readFile('expectedPlain.txt'.trim());
-const plainDataYaml = readFile('expectedPlain.txt'.trim());
-const jsonFormat = readFile('expectedJson.txt'.trim());
+// const stylishData = readFile('expectedStylish.txt'.trim());
+// const stylishDataYaml = readFile('expectedstylish.txt'.trim());
+// const plainData = readFile('expectedPlain.txt'.trim());
+// const plainDataYaml = readFile('expectedPlain.txt'.trim());
+// const jsonFormat = readFile('expectedJson.txt'.trim());
 test('genDiff, json', () => {
   expect(
     genDiff('fileJ1.json', 'fileJ2.json', 'stylish'),
-  ).toEqual(stylishData);
+  ).toEqual(readFile('expectedStylish.txt'.trim()));
 });
 test('genDiff, yaml', () => {
   expect(
     genDiff('fileYaml1.yaml', 'fileYaml2.yaml', 'stylish'),
-  ).toEqual(stylishDataYaml);
+  ).toEqual(readFile('expectedstylish.txt'.trim()));
 });
 
 test('genDiff, json plain', () => {
   expect(
     genDiff('fileJ1.json', 'fileJ2.json', 'plain'),
-  ).toEqual(plainData);
+  ).toEqual(readFile('expectedPlain.txt'.trim()));
 });
 
 test('genDiff, yml plain', () => {
   expect(
     genDiff('fileY1.yml', 'fileY2.yml', 'plain'),
-  ).toEqual(plainDataYaml);
+  ).toEqual(readFile('expectedPlain.txt'.trim()));
 });
 
 test('genDiff, json JSON', () => {
   expect(
     genDiff('fileJ1.json', 'fileJ2.json', 'json'),
-  ).toEqual(jsonFormat);
+  ).toEqual(readFile('expectedJson.txt'.trim()));
 });
 
 test('genDiff, yml JSON', () => {
   expect(
     genDiff('fileYaml1.yml', 'fileYaml2.yml', 'json'),
-  ).toEqual(jsonFormat);
+  ).toEqual(readFile('expectedJson.txt'.trim()));
 });
